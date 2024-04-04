53fbb6863654af121935510207b51069e6ac3b3e
cbeatty42 <37245967+cbeatty42@users.noreply.github.com>
2024-04-04 09:02:22 -0400

    New Serial Port
    Updated serial port used to ACM1 for the Arduino.
    Did slight CPU optimization for reading from the queue

--- motor 5 mode.py
+++ motor 5 mode.py
@@ -4,7 +4,7 @@ import queue
 import time
 
 class ArduinoCommunication:
-    def __init__(self, port='/dev/ttyUSB0', baudrate=9600):
+    def __init__(self, port='/dev/ttyACM1', baudrate=9600):
         self.ser = serial.Serial(port, baudrate)
         self.read_queue = queue.Queue()
         self.write_queue = queue.Queue()
@@ -17,10 +17,8 @@ class ArduinoCommunication:
                 
     def write_to_arduino(self):
         while True:
-            if not self.write_queue.empty():
-                message = self.write_queue.get()
-                self.ser.write(message.encode('utf-8'))
-                time.sleep(1)  # Optional delay
+            message = self.write_queue.get()
+            self.ser.write(message.encode('utf-8'))
 
     def start_communication(self):
         read_thread = threading.Thread(target=self.read_from_arduino)
@@ -40,23 +38,28 @@ if __name__ == "__main__":
             if not arduino.read_queue.empty():
                 received_message = arduino.read_queue.get()
                 print(f'Received: {received_message}')
+            
+            control_value = int(input("Enter an integer 1-5 for motor control"))
+            print("Control value", control_value)
 
             # Example: write to Arduino
-            if received_message == 1:#condition 1
+            if control_value == 1:#condition 1
                 message_to_send = "D1"
                 arduino.write_queue.put(message_to_send)
-            elif received_message == 2:#condition 2
+            elif control_value == 2:#condition 2
                 message_to_send = "D2"
                 arduino.write_queue.put(message_to_send)
-            elif received_message == 3:#condition 3
+            elif control_value == 3:#condition 3
                 message_to_send = "D3"
                 arduino.write_queue.put(message_to_send)
-            elif received_message == 4:#condition 4
+            elif control_value == 4:#condition 4
                 message_to_send = "D4"
                 arduino.write_queue.put(message_to_send)
-            else:#condition 5
+            elif control_value == 5:#condition 5
                 message_to_send = "D5"
                 arduino.write_queue.put(message_to_send)
+            else:
+                print("Value must be between 1 and 5")
 
     except KeyboardInterrupt:
         print("Keyboard interrupt detected. Exiting...")
